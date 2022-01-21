fb959ee6a9e26ed4f8bfec3b3558c1ae0229ae2e
Kalpak <kalpakto26@gmail.com>
2022-01-22 01:39:10 +0530

    Fixed isAnimOne

--- script.js
+++ script.js
@@ -3,15 +3,20 @@ let dollar = document.getElementById("dollar");
 let isAnimOne = true;
 
 setInterval(() => {
+	console.log(isAnimOne);
 	if (isAnimOne) {
-		dollarWrapper.classList.remove("dollar-wrapper-anim1");
-		dollar.classList.remove("dollar-anim1");
+		isAnimOne = !isAnimOne;
 		dollarWrapper.classList.add("dollar-wrapper-anim2");
 		dollar.classList.add("dollar-anim2");
+		// dollarWrapper.classList.remove("dollar-wrapper-anim1");
+		// dollar.classList.remove("dollar-anim1");
 	} else {
-		dollarWrapper.classList.remove("dollar-wrapper-anim2");
-		dollar.classList.remove("dollar-anim2");
-		dollarWrapper.classList.add("dollar-wrapper-anim1");
-		dollar.classList.add("dollar-anim1");
+		isAnimOne = !isAnimOne;
+		dollarWrapper.classList.remove("dollar-wrapper-anim1");
+		dollar.classList.remove("dollar-anim1");
+		// dollarWrapper.classList.remove("dollar-wrapper-anim2");
+		// dollar.classList.remove("dollar-anim2");
+		// dollarWrapper.classList.add("dollar-wrapper-anim1");
+		// dollar.classList.add("dollar-anim1");
 	}
 }, 5000);
